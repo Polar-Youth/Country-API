39731f4b592eb260d948ad165c4e35a98127239b
Tjoosten <Tjoosten3@gmail.com>
2017-03-19 02:25:52 +0100

    Update

---
+++ database/seeds/LanguageTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Class LanguageTableSeeder
+ */
+class LanguageTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // REF: https://nl.wikipedia.org/wiki/Lijst_van_ISO_639-codes
+    }
+}
